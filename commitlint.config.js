90439868582a47a581fdc24c8fad93249f49f3a5
Manoj Mehra <manoj.mehra@spraxa.com>
2024-09-28 13:51:50 +0530

    commit to bypass commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-      'subject-empty': [2, 'never'],
-      'type-empty': [2, 'never'],
-    },
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+};
